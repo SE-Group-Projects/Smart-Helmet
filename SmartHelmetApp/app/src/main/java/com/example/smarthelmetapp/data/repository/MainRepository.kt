5906b1032bfceac4980e293badb86826c291b18b
Kushd70 <abeykoonkushan@gmail.com>
2025-06-30 15:33:43 +0530

    making the structure

---
+++ SmartHelmetApp/app/src/main/java/com/example/smarthelmetapp/data/repository/MainRepository.kt
@@ -0,0 +1,7 @@
+package com.example.smarthelmetapp.data.repository
+
+class MainRepository {
+    fun getInitialMessage(): String{
+        return "Hellow from Repository"
+    }
+}
--- SmartHelmetApp/app/src/main/java/com/example/smarthelmetapp/ui/viewmodel/MainViewModel.kt
+++ SmartHelmetApp/app/src/main/java/com/example/smarthelmetapp/ui/viewmodel/MainViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.smarthelmetapp.ui.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+    // This holds a greeting message
+    private val _greeting = MutableLiveData<String>("HEllow from view model")
+    val greeting: LiveData<String> = _greeting
+
+    // You can later update LiveData when sensor ? AI outputs change...
+    fun updateGreeting(message: String){
+        _greeting.value = message
+    }
 }
