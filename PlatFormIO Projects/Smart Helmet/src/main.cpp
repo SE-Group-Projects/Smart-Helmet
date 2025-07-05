c924bbb9f01a4716529c22812fef91f5db89d7b1
Kushd70 <abeykoonkushan@gmail.com>
2025-07-05 15:26:30 +0530

    checking th GPS

--- PlatFormIO Projects/Smart Helmet/src/main.cpp
+++ PlatFormIO Projects/Smart Helmet/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
-#define  LED 2
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
+
+TinyGPSPlus gps;
+HardwareSerial GPS_Serial(2); 
 
 void setup() {
   Serial.begin(115200);
-  pinMode(2, OUTPUT);  // or try LED_BUILTIN
+  GPS_Serial.begin(9600, SERIAL_8N1, 16, 17); // Rx , TX
+  Serial.println("Waiting for GPS signal.....");
 }
 
 void loop() {
