d7d0f1f09442764e6f2c86fbf1039e9c244d8af9
Kushd70 <abeykoonkushan@gmail.com>
2025-08-27 19:12:02 +0530

    settting up the blyink for th functionality

--- PlatFormIO Projects/Smart Helmet/include/BlynkManager.h
+++ PlatFormIO Projects/Smart Helmet/include/BlynkManager.h
@@ -8,7 +8,20 @@
 #define VPIN_GPS_MAP     V2
 #define VPIN_STATUS_LED  V3
 
+class BlynkManager {
+    public:
+    BlynkManager(const char* authToken, const char* ssid, const char* password);
+    void setup();
+    void run();
+    void updateSystemStatus(bool isOn);
+    void updateSensorData(float temp, double speed, double lat, double lon, bool isGpsValid);
+    void sendCollisionNotification();
 
+private:
+    const char* _authToken;
+    const char* _ssid;
+    const char* _password;
+}
 
 
 
