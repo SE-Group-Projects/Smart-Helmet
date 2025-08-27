61d7dd01a80c8112b717821fa4e1b35690d90326
Kushd70 <abeykoonkushan@gmail.com>
2025-08-27 19:28:59 +0530

    blynk connection

---
+++ PlatFormIO Projects/Smart Helmet/src/BlynkManager.cpp
@@ -0,0 +1,24 @@
+#include "BlynkManager.h"
+
+BlynkManager::BlynkManager(const char* authToken, const char* ssid, const char* password)
+    : _authToken(authToken), _ssid(ssid), _password(password) {}
+
+void BlynkManager::setup(){
+    Serial.print("Connecting to WIFI : ");
+    Serial.println(_ssid);
+    WiFi.begin(_ssid, _password);
+    while(WiFi.status() != WL_CONNECTED){
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nWiFi connected!");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Blynk.config(_authToken);
+    Serial.println("Blynk Manager initialized.");
+}
+
+void BlynkManager::run() {
+    Blynk.run();
+}
