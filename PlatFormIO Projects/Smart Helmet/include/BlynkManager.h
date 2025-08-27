fab0c41843ec3830af67ad94a6fdabdc6bfaf691
Kushd70 <abeykoonkushan@gmail.com>
2025-08-27 19:11:29 +0530

    settting up the blyink for th functionality

---
+++ PlatFormIO Projects/Smart Helmet/include/BlynkManager.h
@@ -0,0 +1,15 @@
+#ifndef BLYNK_MANAGER_H
+#define BLYNK_MANAGER_H
+
+#include <BlynkSimpleEsp32.h>
+
+#define VPIN_TEMPERATURE V0
+#define VPIN_SPEED       V1
+#define VPIN_GPS_MAP     V2
+#define VPIN_STATUS_LED  V3
+
+
+
+
+
+#endif
