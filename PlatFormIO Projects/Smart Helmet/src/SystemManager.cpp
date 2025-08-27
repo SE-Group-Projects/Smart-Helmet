d24a14d4a2541530b7a4a4a06e9a30217646c70c
Kushd70 <abeykoonkushan@gmail.com>
2025-08-27 10:19:47 +0530

    system manager . cpp file

--- PlatFormIO Projects/Smart Helmet/src/SystemManager.cpp
+++ PlatFormIO Projects/Smart Helmet/src/SystemManager.cpp
@@ -12,3 +12,28 @@ void SystemManager::setup(){
     Serial.println("System Manager Initialized . Waiting for Pressure...");
 }
 
+void SystemManager::update(){
+    int fsrReading = analogRead(_fsrPin);
+
+    if (fsrReading > _pressureThreshold){
+        if (!_isSystemOn){
+             _isSystemOn = true;
+            Serial.println("---------------------------------");
+            Serial.print("Pressure detected! System is ON. (Reading: ");
+            Serial.print(fsrReading);
+            Serial.println(")");
+            Serial.println("---------------------------------");
+        }
+        _lastPressureTime = millis();
+    }else {
+        if (_isSystemOn && (millis() - _lastPressureTime > _shutdownTimeout)){
+            _isSystemOn = false;
+            Serial.println("---------------------------------");
+            Serial.println("No pressure for 1 minute. System is SHUTDOWN.");
+            Serial.println("---------------------------------");
+        }
+    }
+}
+bool SystemManager::isSystemOn() const {
+        return _isSystemOn;
+    }
