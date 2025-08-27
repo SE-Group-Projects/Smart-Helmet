97ff4ee0f4f4abe3e30dbb4f52487edbc2c810f8
Kushd70 <abeykoonkushan@gmail.com>
2025-08-27 13:29:02 +0530

    new servo controller

---
+++ PlatFormIO Projects/Smart Helmet/include/VentController.h
@@ -0,0 +1,21 @@
+#ifndef VENT_CONTROLLER_H
+#define VENT_CONTROLLER_H
+
+#include <ESP32Servo.h>
+
+class VentController {
+    public:
+        VentController(int servoPin, float tempThreshold, int openAngle, int closedAngle);
+        void setup();
+        void update(float currentTemperature); // get the temp..........
+
+    private:
+        Servo _servo;
+        int _servoPin;
+        float _tempThreshold;
+        int _openAngle;
+        int _closedAngle;
+        bool _isVentOpen;
+};
+
+#endif
